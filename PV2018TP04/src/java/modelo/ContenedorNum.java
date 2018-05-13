131f13a17d1ec28a808ff3e1ed6e864cd229d614
Judith Aldonate <Judith Aldonate@DESKTOP-T25FIQE>
2018-05-13 19:47:06 -0300

    Punto 2(Duran)

---
+++ PV2018TP04/src/java/modelo/ContenedorNum.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Judith Aldonate
+ */
+public class ContenedorNum {
+    public class ContenedorNumero {
+    private int numero;
+    private int menor;
+    private int mayor;
+    
+    public ContenedorNumero() {
+    }
+
+    public ContenedorNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getMenor() {
+        return menor;
+    }
+
+    public void setMenor(int menor) {
+        this.menor = menor;
+    }
+
+    public int getMayor() {
+        return mayor;
+    }
+
+    public void setMayor(int mayor) {
+        this.mayor = mayor;
+    }
+
+ 
+    
+    public void cargarNumero(int n , ArrayList<Integer> lista){
+       this.numero=n;
+       lista.add(numero);
+       
+    }
+    public int calcularMenor(ArrayList<Integer> lista){
+      menor=lista.get(0);
+      for(int i : lista)
+      {
+          if(i<menor)
+          menor=i;
+      }
+    return menor;
+    }
+    public int calcularMayor(ArrayList<Integer> lista){
+     mayor=lista.get(0);
+     for(int i : lista)
+      {
+       if(i>mayor)
+        mayor=i;
+             
+      }
+      return mayor;
+    }
+    public double calcularPromedio(ArrayList<Integer> lista){
+      
+      int suma=0;
+      for(int i : lista)
+      {
+        suma=suma+i;
+      }
+      return suma/lista.size();
+    }
+    
+}
+    
+}
